21dfe0bf502fd2fcdf8c91ed6e01a3acbabc7fa6
Chengxi Yao <andy-yao@foxmail.com>
2018-04-08 22:30:12 -0400

    modify some format

--- src/app/pages/maps/gmaps/gmaps.component.ts
+++ src/app/pages/maps/gmaps/gmaps.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MapsAPILoader } from '@agm/core';
+// import { FormControl } from '@angular/forms';
+// import { MapsAPILoader } from '@agm/core';
 import { } from 'googlemaps';
 
 @Component({
