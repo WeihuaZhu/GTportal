8d850351256602b971a9275b7c2472d22e3853e9
HaiyueYin <31355802+HaiyueYin@users.noreply.github.com>
2018-04-10 14:25:28 -0400

    yhy

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <span class="created-by"><b><a href="https://akveo.com" target="_blank"></a></b></span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://github.com/" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://www.facebook.com/" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="https://twitter.com/Twitter" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://www.linkedin.com/" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })
