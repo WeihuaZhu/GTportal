3936c8c81d72873859d16a9312b73c19ce265b65
Weihua Zhu <whzhu@gatech.edu>
2018-04-15 18:15:53 -0400

    tinychanges

--- src/app/pages/maps/search-map/entity/Location.ts
+++ src/app/pages/maps/search-map/entity/Location.ts
@@ -1,4 +1,4 @@
 export class Location {
-  constructor(public latitude: number = 53.9, public longitude: number = 27.5667) {
+  constructor(public latitude: number = 33.7756178, public longitude: number = -84.3962850) {
   }
 }
